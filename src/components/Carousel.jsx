81b367a3294ee915834f99edfaf2c58243469433
EwenLeFloch <ewenlelf@gmail.com>
2023-11-21 17:08:58 +0100

    mv images folder

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -62,7 +62,7 @@ const Carousel = ({ items }) => {
 					</div>
 					<img
 						className="carousel__image carousel__image--formation"
-						src="assets/images/openclassrooms.webp"
+						src="../assets/images/openclassrooms.webp"
 						alt="Ewen Le Floch"
 					></img>
 				</div>
