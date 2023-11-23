4361e21b87d4679196c680e80ec56a0fcccf7b90
EwenLeFloch <ewenlelf@gmail.com>
2023-11-23 13:44:27 +0100

    json update and github pages deployment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 
 const App = () => {
 	return (
-		<BrowserRouter basename="/Portfolio">
+		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Home />}></Route>
 				<Route path="*" element={<Navigate to="/" />}></Route>
